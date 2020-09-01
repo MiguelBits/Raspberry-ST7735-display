33c2d22679972676840721967dfbdac3f162cbe4
Miguel de Vasconcelos <mcv.cooper@gmail.com>
2020-09-02 00:52:10 +0100

    pygame display

--- menus/videogames/stickman.py
+++ menus/videogames/stickman.py
@@ -1,1 +1,11 @@
-print "hello"
+import sys,pygame
+
+pygame.init()
+
+background = pygame.image.load('background1.jpg')
+size = (128,128)
+background = pygame.transform.scale(background, size)
+while 1:
+    screen = pygame.display.set_mode(size)
+    screen.blit(background,(0,0))
+    pygame.display.update()

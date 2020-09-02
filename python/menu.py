3dc5d737462a5e5c901025bce9ec6d001560edff
Miguel de Vasconcelos <mcv.cooper@gmail.com>
2020-09-02 17:54:57 +0100

    menu updated - clock works + second stage menu

--- python/menu.py
+++ python/menu.py
@@ -30,21 +30,16 @@ GPIO.setup(KEY1_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with
 GPIO.setup(KEY2_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
 GPIO.setup(KEY3_PIN,        GPIO.IN, pull_up_down=GPIO.PUD_UP)      # Input with pull-up
 
-def time_white(*args, **kwargs):
-        args[0].text((80, 118), "00:00:00", fill = "WHITE")
+def time_white():
+        global draw
+        draw.text((80, 118), "00:00:00", fill = "WHITE")
 
 
 def folder_open(what):
-        draw = ImageDraw.Draw(image)
-        #draw.rectangle((0,0,width,height), outline=0, fill=0)
         mypath =  "/boot/led-hat/code/menus/%s"%(what)
         onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-        c = 10
-        for o in onlyfiles:
-                draw.text((15, c), str(o), fill = "WHITE")
-                c += 10
-        print onlyfiles
+        return onlyfiles
 def setup():
 
         # 240x240 display with hardware SPI:
@@ -57,14 +52,13 @@ def setup():
         return disp,image
 
 def main_menu(disp,image):
-        width = 128; height = 128;
-        # Get drawing object to draw on image.
-        draw = ImageDraw.Draw(image)
+
+        global draw
         # Draw a black filled box to clear the image.
         draw.rectangle((0,0,width,height), outline=0, fill=0)
 
         #Draw time background
-        time_white = draw.rectangle((128,128,69,118),outline = "WHITE",fill="WHITE")
+        draw.rectangle((128,128,69,118),outline = "WHITE",fill="WHITE")
 
         draw.text((15, 10), 'Video Games', fill = "WHITE")
         draw.text((15, 20), 'Apps & tools', fill = "WHITE")
@@ -73,11 +67,11 @@ def main_menu(disp,image):
         i = 0
 
         while 1:
-                #draw.line((50, 50), fill=('BLACK'))
+
+                #Draw Time
+                threading.Timer(1,time_white).start()
                 now = datetime.now()
                 current_time = now.strftime("%H:%M:%S")
-                #threading.Timer(0.8,time_white,args=draw).start()
-
                 draw.text((80, 118), str(current_time), fill = "RED")
 
                 if i <= 0 :
@@ -88,6 +82,7 @@ def main_menu(disp,image):
                         i -= 1
                 if GPIO.input(KEY_DOWN_PIN) == 0:
                         i += 1
+
                 if i == 1: # button is released
                         draw.text((15, 10), 'Video Games', fill = "RED")
                         if GPIO.input(KEY_RIGHT_PIN) == 0:
@@ -103,18 +98,46 @@ def main_menu(disp,image):
 
                 disp.LCD_ShowImage(image,0,0)
 
+def open_menu(arr,image,disp):
+
+        global draw
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0,0,128,128), outline=0, fill=0)
+
+        #Draw time background
+        draw.rectangle((128,128,69,118),outline = "WHITE",fill="WHITE")
+
+        draw.text((15, 10), str(arr[0]), fill = "WHITE")
+        draw.text((15, 20), str(arr[1]), fill = "WHITE")
+        disp.LCD_ShowImage(image,0,0)
+
+        i = 0
+
+        while 1:
+
+                #Draw Time
+                threading.Timer(1,time_white).start()
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                draw.text((80, 118), str(current_time), fill = "RED")
+
+
+                disp.LCD_ShowImage(image,0,0)
+
 
 #MAIN CODE
 disp,image = setup()
-
+width = 128; height = 128;
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
 print "**STARTING**"
 
-while 1:
-        i = main_menu(disp,image)
-        time.sleep(2)
-        disp.LCD_Clear()
-
-        if i == 1:
-                folder_open("videogames")
-        if i == 2:
-                folder_open("appstools")
+i = main_menu(disp,image)
+time.sleep(1)
+disp.LCD_Clear()
+if i == 1:
+        arr = folder_open("videogames")
+        open_menu(arr,image,disp)
+if i == 2:
+        arr = folder_open("appstools")
+        open_menu(arr,image,disp)
